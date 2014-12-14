9d5ca3400533584ee71354873b5ce67138e18e78
Daniel Leary <daniel.leary14@imperial.ac.uk>
2014-12-14 16:57:42 +0000

    fixed pawn

--- Pawn.cpp
+++ Pawn.cpp
@@ -29,10 +29,10 @@ void Pawn::valid_moves(ChessBoard *cb, string square, set<string>& valid_list)
 		take_right = cb->change_square(square, 1, -1);
 	}
 
-	if (is_valid_move(cb, two_forward) && first_turn)
+	if (is_valid_move(cb, two_forward) && first_turn && !is_opponent_piece(cb,two_forward))
 		valid_list.insert(two_forward);
 
-	if (is_valid_move(cb, forward))
+	if (is_valid_move(cb, forward) && !is_opponent_piece(cb,forward))
 		valid_list.insert(forward);
 
 	if (is_valid_move(cb, take_left) && is_opponent_piece(cb, take_left))
